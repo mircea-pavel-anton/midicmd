a1edcf52e2963570fc5079eb25e9099800ffc605
Mircea Anton <mircea.anton99@yahoo.com>
2021-03-31 19:32:15 +0300

    Basic structure setup

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "objects/utils/utils.hpp"
+
+int main(int argc, char **argv) {
+    std::cout << "Hello from midicmd!" << std::endl;
+
+    return 0;
+} //main()
---
+++ src/objects/utils/utils.hpp
@@ -0,0 +1,8 @@
+#ifndef UTILS_CLASS
+#define UTILS_CLASS
+
+class Utils {
+
+}; //Utils
+
+#endif //UTILS_CLASS
