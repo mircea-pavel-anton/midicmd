9a0ecb7ecc6cda9d4a7fbc747ffc10fb60c9d6c7
Mircea Anton <mircea.anton99@yahoo.com>
2021-03-31 23:56:33 +0300

    Add colors namespace for color font in terminal

--- src/objects/Utils/utils.hpp
+++ src/objects/Utils/utils.hpp
@@ -1,6 +1,40 @@
 #ifndef UTILS_CLASS
 #define UTILS_CLASS
 
+namespace Colors {
+	namespace Foreground {
+		static const char* Black 	= "\033[1;30m";
+		static const char* Red 	 	= "\033[1;31m";
+		static const char* Green   	= "\033[1;32m";
+		static const char* Yellow  	= "\033[1;33m";
+		static const char* Blue	 	= "\033[1;34m";
+		static const char* Magenta 	= "\033[1;35m";
+		static const char* Cyan	 	= "\033[1;36m";
+		static const char* White	= "\033[1;37m";
+	} //Foreground
+
+	namespace Background {
+		static const char* Black 	= "\033[1;40m";
+		static const char* Red 	 	= "\033[1;41m";
+		static const char* Green   	= "\033[1;42m";
+		static const char* Yellow  	= "\033[1;43m";
+		static const char* Blue	 	= "\033[1;44m";
+		static const char* Magenta 	= "\033[1;45m";
+		static const char* Cyan	 	= "\033[1;46m";
+		static const char* White	= "\033[1;47m";
+	} //Background
+
+	namespace Modifiers {
+		static const char *Reset 		 = "\033[1;0m";
+		static const char *Bright 		 = "\033[1;1m";
+		static const char *Underline 	 = "\033[1;4m";
+		static const char *Inverse 		 = "\033[1;7m";
+		static const char *Bright_Off 	 = "\033[1;21m";
+		static const char *Underline_Off = "\033[1;24m";
+		static const char *Inverse_Off 	 = "\033[1;27m";
+	} //Modifiers
+} //Colors
+
 class Utils {
 
 }; //Utils
