0853caa3017e26293f53b2ecccbc46de4130b225
Mircea Anton <mircea.anton99@yahoo.com>
2021-04-02 01:12:14 +0300

    Convert indentation to tabs

--- src/objects/Device/device.hpp
+++ src/objects/Device/device.hpp
@@ -4,24 +4,24 @@
 #include "../Utils/utils.hpp"
 
 class Device {
-    public:
-        int portId;
-        string name;
-        bool isSet;
+	public:
+		int portId;
+		string name;
+		bool isSet;
 
-        Device() : portId(-1), name(""), isSet(false) {}
-        Device(int _id, string _name) : portId(_id), name(_name), isSet(true) {}
+		Device() : portId(-1), name(""), isSet(false) {}
+		Device(int _id, string _name) : portId(_id), name(_name), isSet(true) {}
 
-        void clear() {
-            portId = -1;
-            name = "";
-            isSet = false;
-        }
-        void set(int _id, string _name) {
-            portId = _id;
-            name = _name;
-            isSet = true;
-        }
+		void clear() {
+			portId = -1;
+			name = "";
+			isSet = false;
+		}
+		void set(int _id, string _name) {
+			portId = _id;
+			name = _name;
+			isSet = true;
+		}
 };
 
 #endif //DEVICE_CLASS
