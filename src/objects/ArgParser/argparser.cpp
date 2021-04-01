f0a38aa0e27bc00fc8be37f4efa86d87b32b29d8
Mircea Anton <mircea.anton99@yahoo.com>
2021-04-02 01:02:38 +0300

    Condense statements into one line wherever possible

--- src/objects/ArgParser/argparser.cpp
+++ src/objects/ArgParser/argparser.cpp
@@ -1,11 +1,7 @@
 #include "argparser.hpp"
 
-ArgParser::ArgParser() {
-	cmdHelper = new CmdHelper();
-}
-ArgParser::~ArgParser() {
-	delete cmdHelper;
-}
+ArgParser::ArgParser() { cmdHelper = new CmdHelper(); }
+ArgParser::~ArgParser() { delete cmdHelper; }
 
 void ArgParser::parse(int argc, char **argv) {
 	if (argc < 2) {
@@ -17,25 +13,11 @@ void ArgParser::parse(int argc, char **argv) {
 	if (command.compare("device") == 0) return parse_device(argc, argv);
 	if (command.compare("config") == 0) return parse_config(argc, argv);
 
-	if (command.compare("start") == 0) {
-		cmdHelper->Start();
-		return;
-	}
+	if (command.compare("start") == 0) return cmdHelper->Start();
+	if (command.compare("stop") == 0) return cmdHelper->Stop();
+	if (command.compare("status") == 0) return cmdHelper->Status();
 
-	if (command.compare("stop") == 0) {
-		cmdHelper->Stop();
-		return;
-	}
-
-	if (command.compare("status") == 0) {
-		cmdHelper->Status();
-		return;
-	}
-
-	if (command.compare("help") == 0) {
-		cmdHelper->help();
-		return;
-	}
+	if (command.compare("help") == 0) return cmdHelper->help();
 
 	// if we got this far, then argv[1] was an unknown command
 	cout << toRed("Unknown command!") << endl;
@@ -49,20 +31,14 @@ void ArgParser::parse_device(int argc, char **argv) {
 	}
 	string command = argv[2];
 
-	if (command.compare("ls") == 0) {
-		cmdHelper->deviceList();
-		return;
-	}
-
-	if (command.compare("ps") == 0) {
-		cmdHelper->deviceStatus();
-		return;
-	}
+	if (command.compare("ls") == 0) return cmdHelper->deviceList();
+	if (command.compare("ps") == 0) return cmdHelper->deviceStatus();
+	if (command.compare("rm") == 0) return cmdHelper->deviceRemove();
 
 	if (command.compare("set") == 0) {
 		if (argc == 4) { // midicmd device set X
 			try {
-				uint16_t arg_value = std::stoi(argv[3]);
+				const int arg_value = std::stoi(argv[3]);
 				cmdHelper->deviceSet(arg_value);
 			} catch (std::invalid_argument &err) {
 				cout << toRed("Invalid argument!") << endl;
@@ -74,15 +50,7 @@ void ArgParser::parse_device(int argc, char **argv) {
 		return;
 	}
 
-	if (command.compare("rm") == 0) {
-		cmdHelper->deviceRemove();
-		return;
-	}
-
-	if (command.compare("help") == 0) {
-		cmdHelper->deviceHelp();
-		return;
-	}
+	if (command.compare("help") == 0) return cmdHelper->deviceHelp();
 
 	// if we got this far, then argv[1] was an unknown command
 	cout << toRed("Unknown command!") << endl;
@@ -119,4 +87,8 @@ void ArgParser::parse_config(int argc, char **argv) {
 		cout << "config help" << endl; //TODO
 		return;
 	}
+
+	// if we got this far, then argv[1] was an unknown command
+	cout << toRed("Unknown command!") << endl;
+	cout << "Run " << toYellow("midicmd config help") << " for more details." << endl;
 }
