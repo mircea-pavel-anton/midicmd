29df77808e959e4821ad60ccf8101e73353ef5c5
Mircea Anton <mircea.anton99@yahoo.com>
2021-04-01 03:01:42 +0300

    Remove `std::` namespace modifier & update help

--- src/objects/CmdHelper/cmdhelper_device.cpp
+++ src/objects/CmdHelper/cmdhelper_device.cpp
@@ -1,14 +1,13 @@
 #include "cmdhelper.hpp"
 
 uint16_t CmdHelper::deviceList() {
-	std::vector<std::string> devices = midiHelper->getInputDevices();
+	vector<string> devices = midiHelper->getInputDevices();
 	const uint16_t device_count = devices.size();
 
-	std::cout << Colors::Foreground::Green;
-	std::cout << "There are " << device_count << " devices available:";
-	std::cout << Colors::Modifiers::Reset << std::endl;
+	cout << toGreen( "There are " + std::to_string(device_count) + " devices available:" );
+	cout << endl;
 	for (uint16_t i = 0; i < device_count; i++) {
-		std::cout << "\t" << i << ") " << devices[i] << std::endl;
+		cout << "\t" << i << ") " << devices[i] << endl;
 	}
 
 	return device_count;
@@ -18,10 +17,10 @@ void CmdHelper::deviceSet() {
 	uint16_t device_count = deviceList();
 	uint16_t user_input;
 
-	std::cout << std::endl;
+	cout << endl;
 	do {
-		std::cout << "Choose your device [0-" << device_count-1 << "]: ";
-		std::cin >> user_input;
+		cout << "Choose your device [0-" << device_count-1 << "]: ";
+		cin >> user_input;
 	} while (user_input < 0 || user_input >= device_count);
 
 	deviceSet(user_input);
@@ -30,31 +29,28 @@ void CmdHelper::deviceSet() {
 void CmdHelper::deviceSet(uint16_t port_id) {
 	try {
 		midiHelper->setInputDevice(port_id);
-		std::cout << Colors::Foreground::Green;
-		std::cout << "Device changed!";
-		std::cout << Colors::Modifiers::Reset << std::endl;
+		cout << toGreen("Device changed!") << endl;
 	} catch(std::range_error &err) {
-		std::cout << Colors::Foreground::Red;
-		std::cout << "Failed to set device. Index out of range!";
-		std::cout << Colors::Modifiers::Reset << std::endl;
+		cout << toRed("Failed to set device. Index out of range!") << endl;
 	}
 }
 
 void CmdHelper::deviceRemove() {
 	midiHelper->clearDevice();
-	std::cout << Colors::Foreground::Green;
-	std::cout << "Device cleared!";
-	std::cout << Colors::Modifiers::Reset << std::endl;
+	cout << toGreen("Device cleared!") << endl;
 }
 
 void CmdHelper::deviceHelp() {
-	std::cout << Colors::Foreground::Yellow;
-	std::cout << "Usage: ";
-	std::cout << Colors::Modifiers::Reset;
-	std::cout << "midicmd device [OPTION]" << std::endl << std::endl;
+	cout << toYellow("Usage: ") << "midicmd device [OPTION]" << endl;
+	cout << endl;
 
-	std::cout << "Available options: " << std::endl;
-	std::cout << "\tls" << "\tPrint a list of all available devices" << std::endl;
-	std::cout << "\tset" << "\tChoose one of the available input devices for this program to listen to." << std::endl;
-	std::cout << "\trm" << "\tRemove the current selection of active device, if any" << std::endl;
+	cout << "Available options: " << endl;
+	cout << toBold("    ls\t\t") << "Shows a list of all available devices." << endl;
+	cout << toBold("    set\t\t") << "Shows a list of all available devices and then prompts" << endl;
+	cout << "\t\t\t" << "you to pick one of them." << endl;
+	cout << toBold("    set [VALUE]\t") << "Sets the active device based on the given id." << endl;
+	cout << "\t\tThe id is the number associated to the device," << endl;
+	cout << "\t\tas shown via the `ls` option." << endl;
+	cout << toBold("    rm\t\t") << "Remove the current selection of active device, if any." << endl;
+	cout << endl;
 }
