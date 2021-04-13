4e4cf881508f5100e4da2a9ee8155697618165e9
Mircea Anton <mircea.anton99@yahoo.com>
2021-04-13 15:10:32 +0300

    Implement the add command method

--- src/objects/CommandHelper/command_helper.cpp
+++ src/objects/CommandHelper/command_helper.cpp
@@ -7,7 +7,58 @@ CommandHelper::CommandHelper(MidiHelper *midi, ConfigHelper *config) {
 CommandHelper::~CommandHelper() { /* we don't delete here, but in ArgParser */ }
 
 void CommandHelper::add() {
-	//TODO
+	if (!isRoot()) {
+		std::cout << toRed("Adding a command requires sudo privileges in order to edit the config file!");
+		std::cout << std::endl;
+		return;
+	}
+
+	int midi_port = midiHelper->getInputDeviceId( configHelper->getDevice() );
+	if (midi_port < 0) {
+		std::cout << toRed("Invalid device specified in the config file!") << std::endl;
+		std::cout << "You can set a new device using " << toYellow("sudo midicmd device set") << std::endl;
+		return;
+	}
+
+	midiHelper->setInputDevice(midi_port);
+	std::string command = "";
+	bool status = false;
+	MidiEvent event;
+	auto commands = configHelper->getCommands();
+
+	std::cout << "Press the key on your midi device you want to bind a macro to:" << std::endl;
+	do {
+		event = midiHelper->getMessage();
+
+		if (event.isOk()) {
+			std::cout << "Event: " << event.getEventName() << std::endl;
+			std::cout << "Channel: " << event.getChannel() << std::endl;
+			std::cout << "Note: " << event.getNoteName() << std::endl << std::endl;
+
+			status = getYesNo("Is this ok?");
+
+			if (status == true) {
+				if (commands.find(event.getCode()) != commands.end()) {
+					std::cout << toYellow("A macro is already bound to this key!") << std::endl;
+					std::cout << "By continuing, you will OVERWRITE the existing macro." << std::endl;
+					status = getYesNo("Continue?");
+					
+					if (status) commands.erase(event.getCode());
+				}
+			}
+
+			if (status == false) std::cout << "Press another key: " << std::endl;
+		}
+	} while (status == false);
+
+	std::cout << "Type in the command you wish this macro to run: " << std::endl;
+	do {
+		std::getline(std::cin, command);
+	} while(command.size() <= 0);
+
+	std::pair<int, const char*> entry = { event.getCode(), command.c_str() };
+	commands.emplace(entry);
+	configHelper->setCommands(commands);
 }
 
 void CommandHelper::list() {
@@ -35,7 +86,7 @@ void CommandHelper::remove() {
 
 	std::map<int, const char*> commands = configHelper->getCommands();
 	if (commands.size() == 0) {
-		std::cout << toYellow("There are no commands configured.") << std::endl;
+		std::cout << toRed("There are no commands configured.") << std::endl;
 		std::cout << toYellow("There is nothing to delete.") << std::endl;
 		return;
 	}
